9c0d7e7d5d02c010bd4b2a8acebd846109d77579
Parisamazinanian <par.mazinanian@gmail.com>
2021-12-10 14:15:47 +0100

    Updated

---
+++ src/test/java/correct/TestSuit.java
@@ -0,0 +1,7 @@
+package correct;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+@Suite
+@SelectClasses({TestEmpListSize.class, TestEmpShuffle.class})
+public class TestSuit {
+}
